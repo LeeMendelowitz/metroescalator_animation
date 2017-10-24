868974168afaaac1b99e32da568c78d6c063c9f5
Lee Mendelowitz <Lee.Mendelowitz@gmail.com>
2017-10-24 19:02:55 -0400

    renamed variables

--- www/funcs.js
+++ www/funcs.js
@@ -1,20 +1,20 @@
-var get_units_by_status_date = function(date_index, status) {
-  var my_date = outage_data.dates[date_index];
-  var unit_index = outage_data.category_by_date[status];
-  unit_index = unit_index  && unit_index[date_index];
+// var get_units_by_status_date = function(date_index, status) {
+//   var my_date = outage_data.dates[date_index];
+//   var unit_index = outage_data.category_by_date[status];
+//   unit_index = unit_index  && unit_index[date_index];
 
-  // For each unit, convert it to a dictionary
+//   // For each unit, convert it to a dictionary
   
-  var units = unit_index.map(function(ind) {
-    return outage_data.units[ind];
-  });
+//   var units = unit_index.map(function(ind) {
+//     return outage_data.units[ind];
+//   });
 
-  return units;
-};
+//   return units;
+// };
 
-var hydrate_units = function(items) {
-  var u = items.units;
-  var u_names = items.units_slots;
+var hydrate_units = function(data) {
+  var u = data.units;
+  var u_names = data.units_slots;
   var hyrdated_units = u.map(function(v, unit_index) {
     var ret = {};
     for(var i = 0; i < v.length; i++) {
@@ -24,7 +24,7 @@ var hydrate_units = function(items) {
     return ret;
   });
 
-  var break_counts = compute_broken_count(items);
+  var break_counts = compute_broken_count(data);
   break_counts.map(function(count, ind) {
     hyrdated_units[ind]['break_count'] = count;
   });
@@ -33,9 +33,9 @@ var hydrate_units = function(items) {
 };
 
 
-var compute_broken_count = function(items) {
-  var ret = Array(items.units.length).fill(0);
-  var break_by_dates = items.category_by_date["BROKEN"];
+var compute_broken_count = function(data) {
+  var ret = Array(data.units.length).fill(0);
+  var break_by_dates = data.category_by_date["BROKEN"];
   break_by_dates.map(function(inds) {
     inds.map(function(i) {
       ret[i] += 1;
